52ef1beb749492c7ec8c898d990cec3f6e89ea64
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-13 18:18:19 -0500

    Connect store and router to app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
-import App from './App'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import rootReducer from './store/reducers';
+
+const store = createStore(rootReducer, {
+  user: {},
+  destinations: [],
+  favorites: [],
+})
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 )
