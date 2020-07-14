929bb9f4e330ecf4883e24830dc6d5d8459dead2
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-13 20:10:29 -0500

    Define endpoint variable url

---
+++ src/services/endpoint.js
@@ -0,0 +1,5 @@
+export const BASE = 'https://next-destination.herokuapp.com/api/v1/';
+export const DEST = 'destinations';
+export const FAV = 'favorites';
+export const USERS = 'users';
+export const AUTH = 'auth';
