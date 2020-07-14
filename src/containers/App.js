96856e78841b393325ed4af8819e70c25baa0d31
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-14 16:05:56 -0500

    Set up connection status verification

---
+++ src/containers/App.js
@@ -0,0 +1,83 @@
+import React, { useCallback } from 'react';
+import { connect } from 'react-redux';
+import { useState } from 'react';
+import Axios from 'axios';
+import * as EP from '../services/endpoint';
+import * as Actions from '../store/actions';
+import Header from '../components/Header';
+import Carousel from '../components/Carousel';
+
+const App = ({
+  changeDestinations,
+  destinations,
+  user,
+  logIn,
+  logOut,
+}) => {
+  // Local State
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  // Handlers
+  const handleFetchDestinations = React.useCallback(() => {
+    setLoading(true);
+
+    Axios.get(`${EP.BASE}${EP.DEST}`)
+      .then(result => {
+        setLoading(false);
+        changeDestinations(result.data.destinations);
+      })
+      .catch(() => {
+        setError(true);
+      });
+  }, [setLoading, changeDestinations, setError]);
+
+  const verifyConnection = useCallback(() => {
+    Axios.get(`${EP.BASE}${EP.AUTH}/connected`)
+      .then(result => {
+        console.log(result.data)
+        if (result.data.connected && !user.connected) {
+          logIn(result.data);
+        } else {
+          logOut();
+        }
+      })
+      .catch(() => setError())
+  }, [logIn, logOut, user.connected]);
+
+  // Effects
+  React.useEffect(() => {
+    verifyConnection();
+  }, [verifyConnection]);
+
+  React.useEffect(() => {
+    handleFetchDestinations();
+  }, [handleFetchDestinations]);
+
+  return (
+    <>
+      {/* {
+        loading ? 'loading' :
+          <Carousel destinations={destinations} />
+      } */}
+    </>
+  )
+};
+
+const mapStateToProps = state => ({
+  user: state.user,
+  destinations: state.destinations,
+  favorites: state.favorites,
+  menu: state.menu,
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeDestinations: data => dispatch(Actions.changeDestinations(data)),
+  changeFavorites: data => dispatch(Actions.changeFavorites(data)),
+  createUser: data => dispatch(Actions.createUser(data)),
+  logIn: data => dispatch(Actions.logIn(data)),
+  logOut: () => dispatch(Actions.logOut()),
+  toggleMenu: () => dispatch(Actions.toggleMenu()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
