73109df28a498cb36dd50f12abfac7c528921ae1
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-15 12:41:23 -0500

    Add link to item details

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Item = ({ destination }) => {
 
   return (
-    <article>
-      <img src="https://picsum.photos/300/450" alt="Img" />
-      <h3>{destination.name}</h3>
-      <p>{destination.favorites_count}</p>
-    </article>
+    <Link to={`/${destination.id}`}>
+      <article>
+        <img src="https://picsum.photos/300/450" alt="Img" />
+        <h3>{destination.name}</h3>
+        <p>{destination.favorites_count}</p>
+      </article>
+    </Link>
   )
 };
 
