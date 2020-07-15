970e79748cbeaf84f0afeb611bd488ac2e0478eb
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-15 12:40:22 -0500

    Connect destination details to component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { connect, useSelector } from 'react-redux';
 
 const Details = props => {
-
+  const { id } = useParams();
+  const destination = useSelector(state =>
+    state.destinations.find(e => e.id === parseInt(id))
+  )
+  console.log(destination)
   return (
     <>
       <section>
         <div>
           <img src="https://picsum.photos/300/280" alt="Img" />
-          <span>Favorites count</span>
+          <span>{destination.favorites_count}</span>
         </div>
         <div>
           <h2>About this destination</h2>
-          <p>Descrition goes here.</p>
+          <p>{destination.description}</p>
         </div>
         <button>Save to favorites</button>
       </section>
