fb769d50901821642e2e28c31e8cdf797d17df4e
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-14 21:27:04 -0500

    Initiate Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = props => {
+
+  return (
+    <header>
+      <span>menu icon</span>
+      <h1>Page name</h1>
+    </header>
+  )
+};
+
+export default Header;
