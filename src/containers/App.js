9b72ad66cdffb7b24e55e97b4f817123e8c02c3f
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-15 19:21:24 -0500

    Replace local loading state with global state Other components depend on the state for properly rendering

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,41 +2,38 @@ import React, { useCallback } from 'react';
 import { connect } from 'react-redux';
 import { useState } from 'react';
 import Axios from 'axios';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as EP from '../services/endpoint';
 import * as Actions from '../store/actions';
-import Header from '../components/Header';
-import Carousel from '../components/Carousel';
 import Login from '../containers/Login';
 import SignUp from '../containers/SignUp';
-import Menu from '../components/Menu';
 import Home from '../containers/Home';
-import Details from '../components/Details';
 
 const App = ({
   changeDestinations,
-  destinations,
   user,
   logIn,
   logOut,
+  loading,
+  startLoading,
+  endLoading,
 }) => {
   // Local State
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   // Handlers
   const handleFetchDestinations = React.useCallback(() => {
-    setLoading(true);
+    startLoading();
 
     Axios.get(`${EP.BASE}${EP.DEST}`)
       .then(result => {
-        setLoading(false);
         changeDestinations(result.data.destinations);
+        endLoading();
       })
       .catch(() => {
         setError(true);
       });
-  }, [setLoading, changeDestinations, setError]);
+  }, [startLoading, endLoading, changeDestinations, setError]);
 
   const verifyConnection = useCallback(() => {
     Axios.get(`${EP.BASE}${EP.AUTH}/connected`)
@@ -61,14 +58,19 @@ const App = ({
 
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/:id" component={user.connected ? Home : Login} />
-          <Route exact path={'/'} component={user.connected ? Home : Login} />
-          <Route path="/login" component={user.connected ? Home : Login} />
-          <Route path="/signup" component={user.connected ? Home : SignUp} />
-        </Switch>
-      </Router>
+      {error ? 'Something went wrong' : null}
+      {
+        loading ? 'Loading content...' :
+          <Router>
+            <Switch>
+              <Route exact path="/:id" component={user.connected ? Home : Login} />
+              <Route exact path={'/'} component={user.connected ? Home : Login} />
+              <Route path="/login" component={user.connected ? Home : Login} />
+              <Route path="/signup" component={user.connected ? Home : SignUp} />
+            </Switch>
+          </Router>
+      }
+
     </div>
   )
 };
@@ -78,15 +80,17 @@ const mapStateToProps = state => ({
   destinations: state.destinations,
   favorites: state.favorites,
   menu: state.menu,
+  loading: state.loading,
 });
 
 const mapDispatchToProps = dispatch => ({
   changeDestinations: data => dispatch(Actions.changeDestinations(data)),
-  changeFavorites: data => dispatch(Actions.changeFavorites(data)),
   createUser: data => dispatch(Actions.createUser(data)),
   logIn: data => dispatch(Actions.logIn(data)),
   logOut: () => dispatch(Actions.logOut()),
   toggleMenu: () => dispatch(Actions.toggleMenu()),
+  startLoading: () => dispatch(Actions.startLoading()),
+  endLoading: () => dispatch(Actions.endLoading()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
