cf0cb6494b6eece3cd2e1ab5a0a6b3ba0882795e
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-15 22:25:42 -0500

    Create handler for log out

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,33 +1,52 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Axios from 'axios';
 import Header from '../components/Header';
 import Menu from '../components/Menu';
 import Carousel from '../components/Carousel';
 import Favorites from '../components/Favorites';
 import Details from '../components/Details';
 import Dashboard from '../components/Dashboard';
+import * as EP from '../services/endpoint';
+import * as Actions from '../store/actions';
 
-const Home = ({ user, destinations }) => (
-  <Router>
-    <Menu user={user} />
-    <Header />
-    <Switch>
-      <Route exact path="/">
-        <Carousel destinations={destinations} />
-      </Route>
-      <Route path="/favorites">
-        <Favorites favorites={user.favorites} />
-      </Route>
-      <Route path="/dashboard" component={user.admin && Dashboard} />
-      <Route exact path="/:id" children={<Details />} />
-    </Switch>
-  </Router>
-);
+const Home = ({ user, destinations, logOut, loading }) => {
+  const history = useHistory();
+  const handleLogOut = () => {
+    Axios.delete(`${EP.BASE}${EP.AUTH}/logout`,
+      { withCredentials: true })
+      .then(() => {
+        logOut();
+        history.push('/login');
+      });
+  }
+  return (
+    <Router>
+      <Menu user={user} handleLogOut={handleLogOut} />
+      <Header />
+      <Switch>
+        <Route path="/favorites">
+          <Favorites favorites={user.favorites} />
+        </Route>
+        <Route path="/dashboard" component={user.admin && Dashboard} />
+        <Route exact path="/:id" component={Details} />
+        <Route path="/">
+          <Carousel destinations={destinations} />
+        </Route>
+      </Switch>
+    </Router>
+  )
+};
 
 const mapStateToProps = state => ({
   user: state.user,
   destinations: state.destinations,
+  loading: state.loading,
 });
 
-export default connect(mapStateToProps, null)(Home);
+const mapDispatchToProps = dispatch => ({
+  logOut: () => dispatch(Actions.logOut()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
