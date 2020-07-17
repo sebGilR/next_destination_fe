18e6ccf8be5f97e8f2dfcae4bc42e55163bfbac2
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-16 21:22:17 -0500

    Include reset css and remove favorites state

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -11,7 +11,7 @@ import Dashboard from '../components/Dashboard';
 import * as EP from '../services/endpoint';
 import * as Actions from '../store/actions';
 
-const Home = ({ user, destinations, logOut, loading }) => {
+const Home = ({ user, destinations, logOut, menu, toggleMenu }) => {
   const history = useHistory();
   const handleLogOut = () => {
     Axios.delete(`${EP.BASE}${EP.AUTH}/logout`,
@@ -20,19 +20,22 @@ const Home = ({ user, destinations, logOut, loading }) => {
         logOut();
         history.push('/login');
       });
-  }
+  };
+
   return (
     <Router>
-      <Menu user={user} handleLogOut={handleLogOut} />
-      <Header />
+      <Menu user={user} handleLogOut={handleLogOut} menu={menu} toggleMenu={toggleMenu} />
       <Switch>
         <Route path="/favorites">
           <Favorites favorites={user.favorites} />
         </Route>
         <Route path="/dashboard" component={user.admin && Dashboard} />
-        <Route exact path="/:id" component={Details} />
+        <Route exact path="/:id">
+          <Details />
+        </Route>
         <Route path="/">
-          <Carousel destinations={destinations} />
+          <Header title="Home" home menu={menu} toggleMenu={toggleMenu} />
+          <Carousel destinations={destinations} menu={menu} />
         </Route>
       </Switch>
     </Router>
@@ -43,10 +46,12 @@ const mapStateToProps = state => ({
   user: state.user,
   destinations: state.destinations,
   loading: state.loading,
+  menu: state.menu,
 });
 
 const mapDispatchToProps = dispatch => ({
   logOut: () => dispatch(Actions.logOut()),
+  toggleMenu: () => dispatch(Actions.toggleMenu()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
