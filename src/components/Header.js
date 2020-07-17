145b54176e7eb6b21fea16659dedadeea3e59059
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-16 21:25:14 -0500

    Set logic for menu or return button, include classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styles from '../assets/style/Header.module.scss';
+import { Link } from 'react-router-dom';
 
-const Header = props => {
+const Header = ({ title, home, toggleMenu, menu }) => {
 
   return (
-    <header>
-      <span>menu icon</span>
-      <h1>Page name</h1>
+    <header className={`${styles.container} ${menu && styles.moved}`.trim()}>
+      {home ?
+        <i className={`fas fa-bars ${styles.icon}`} onClick={toggleMenu}></i> :
+        <Link to="/" className={`${styles.icon}`}>
+          <i className={`fas fa-chevron-left`}></i>
+        </Link>
+      }
+      <h1 className={styles.title}>{title}</h1>
     </header>
   )
 };
