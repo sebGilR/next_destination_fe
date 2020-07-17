575a06b1eb3251ebe1a5ddf7068812ca45746752
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-16 21:24:17 -0500

    Style menu and connect handlers

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from '../assets/style/Menu.module.scss';
 
-const Menu = ({ user, handleLogOut, handleDelete }) => {
+const Menu = ({ user, handleLogOut, handleDelete, menu, toggleMenu }) => {
   return (
-    <aside>
+    <aside className={`${styles.container} ${!menu && styles.hidden}`.trim()}>
       <div>
-        <div>
+        <div className={styles.icon}>
           {user.username[0].toUpperCase()}
         </div>
-        <span>@{user.username}</span>
+        <span className={styles.username}>@{user.username}</span>
       </div>
-      <ul>
-        <li><Link to="/">Home</Link> </li>
-        <li> <Link to="/favorites">Favorites</Link> </li>
+      <ul className={styles.nav}>
+        <li><Link to="/" onClick={toggleMenu}>Home</Link> </li>
+        <li><Link to="/favorites" onClick={toggleMenu}>Favorites</Link> </li>
         {
           user.admin &&
-          <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/dashboard" onClick={toggleMenu}>Dashboard</Link></li>
         }
       </ul>
-      <ul>
-        <li onClick={handleLogOut}>Log out</li>
-        <li onClick={handleDelete}>Delete acount</li>
+      <ul >
+        <li onClick={() => {
+          handleLogOut();
+          toggleMenu();
+        }}>Log out</li>
       </ul>
     </aside>
   )
