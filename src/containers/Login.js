576a080877f3add8d4c0f0c7fb7db3268b8d4152
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-17 15:46:16 -0500

    Style Login and link to SignUp

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,9 +3,14 @@ import { connect } from 'react-redux';
 import Axios from 'axios';
 import * as Actions from '../store/actions';
 import * as EP from '../services/endpoint';
+import { Link } from 'react-router-dom';
+import styles from '../assets/style/Login.module.scss';
 Axios.defaults.withCredentials = true;
 
-const Login = props => {
+const Login = ({
+  logIn,
+  loading,
+}) => {
   const [username, setUsername] = useState('');
   const [error, setError] = useState(false);
 
@@ -20,33 +25,39 @@ const Login = props => {
       username: username,
     })
       .then(result => {
-        props.logIn(result.data);
+        logIn(result.data);
       })
       .catch(() => setError(true));
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
         <h1>Sign in</h1>
         <p>Hello there! Sign in to start making your travel plans!</p>
         <form onSubmit={handleSubmit}>
           <input
+            className={styles.input}
             type="text"
+            name="username"
             value={username}
             placeholder="Enter your username"
             onChange={handleUsername}
           />
           {
-            props.loading ?
-              <p>Signing in...</p> : <button type="submit">Sign in</button>
+            loading ?
+              <p>Signing in...</p> :
+              <button type="submit" className={styles.button}>Sign in</button>
           }
           {
             error ?
-              'There was an error when trying to sign in. Please verify your credentials.' :
+              'Make sure your credentials are correct.' :
               null
           }
-          <p>Don't have an account yet? Sign up</p>
+          <p className={styles.alt}>
+            Don't have an account yet?
+            <Link to="/signup"> Sign up</Link>
+          </p>
         </form>
       </div>
     </div>
@@ -60,8 +71,6 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   logIn: data => dispatch(Actions.logIn(data)),
-  startLoading: () => dispatch(Actions.startLoading()),
-  endLoading: () => dispatch(Actions.endLoading()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
