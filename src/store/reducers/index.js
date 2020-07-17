c9fca15b0b65b969647760db7d8eecbd64b7cdc0
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-16 21:19:43 -0500

    Remove unused reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,14 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import destinationsReducer from './destinationsReducer';
-import favoritesReducer from './favoritesReducer';
 import menuReducer from './menuReducer';
 import loadingReducer from './loadingReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   destinations: destinationsReducer,
-  favorites: favoritesReducer,
   menu: menuReducer,
   loading: loadingReducer,
 });
