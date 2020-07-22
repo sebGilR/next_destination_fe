1f6cfcd635ae71f43484f051fbbdc5678a2bffe5
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-22 14:42:13 -0500

    Fix linting errors and update endpoint base

--- src/services/endpoint.js
+++ src/services/endpoint.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-export const BASE = 'http://127.0.0.1:4000//api/v1/';
+export const BASE = 'https://next-destination.herokuapp.com/api/v1/';
 export const DEST = 'destinations';
 export const FAV = 'favorites';
 export const USERS = 'users';
